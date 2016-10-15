0965366c6467e86ebf3d11109d2f9f67feba40ec
Wolfgang Criollo <wolfplisken@gmail.com>
2016-10-15 13:40:38 -0400

    set ip a basic calculator to a sum of two numbers

---
+++ basic-calculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class BasicCal{
+  public static void main(String[] args) {
+      Scanner wolf = new Scanner(System.in);
+      double fnum, snum, answer;
+
+      System.out.println("Enter first number:");
+      fnum = wolf.nextDouble();
+
+      System.out.println("Enter second number:");
+      snum = wolf.nextDouble();
+
+      answer = fnum  + snum;
+
+      System.out.println(answer);
+  }
+}
